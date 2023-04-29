3637e9dd9b18f28822d105d7255cd75c1c0928e7
TheRocks18 <miguelrc.1809@hotmail.com>
2023-04-30 00:27:47 +0100

    Análise dos dados: Miguel

--- eeg.m
+++ eeg.m
@@ -12,14 +12,15 @@
 % P3 (300-600 ms): FZ, Fz, Pz
 % https://onlinelibrary.wiley.com/doi/pdfdirect/10.1111/j.1469-8986.2007.00602.x
 
-%% Importar os dados
+%% Importar os dados e inicializar variáveis
+
 close all;
 clc, clear;
 
 addpath(genpath('eeglab2023.0')); % Path do EEGLAB
 ch_locs=readlocs('BioSemi64.loc'); % Ficheiro com localização dos elétrodos
 
-data=readtable("S2_21_Male.csv");
+data=readtable("S3_25_Male.csv");
 data=table2array(data,1);
 
 fs = 125;
@@ -34,189 +35,223 @@ for i = 1:length(ch_locs)
     end
 end
 
-nch_locs = ch_locs(logical(idx));
+nch_locs = ch_locs(logical(idx)); % Elétrodos usados
+
+%% Verificar o efeito dos filtros na fase dos sinais
+
+t = (-3:1/fs:3)';
+s = 5*square(2*pi*t); % Sinal com a mesma frequência que o sinal da fotoresistência
+
+sqwave = awgn(s, 20, 'measured'); % Gaussian white noise
+
+plot(t, [sqwave, movmean(sqwave,10), movmedian(sqwave,20)]),
+title('Verificar o efeito dos filtros na fase dos sinais'), 
+xlabel('Tempo (s)'),
+legend('Original','Média flutuante','Mediana flutuante');
+
+% Resumindo, a mediana flutuante não distorce tanto as fases do sinal,
+% principalmente no início das rising edges e falling edges. Isto é
+% extremamente importante em análises como as dos ERPs pois é preciso
+% saber com exatidão o instante em que o estímulo é apresentado.
 
 %% Dados da fotoresistência 
 
 photoVector=(data(:,end));
 
 t = ((0:length(photoVector)-1)*1/fs)/60;
+n_pnts = 20;
+filt_photoVector = movmedian(photoVector,20);
 
-% Janela de tempo
-window_size = 7;
+% lpFilt = designfilt('lowpassfir','FilterOrder',1000,'PassbandFrequency',2, ...
+%          'StopbandFrequency',2.5,'SampleRate',fs);
+%fvtool(lpFilt)
 
-% Padding do sinal
-padded_signal = [zeros(window_size-1, 1); photoVector; zeros(window_size-1, 1)];
-filt_photoVector = zeros(size(photoVector));
-
-for i = 1:length(photoVector)
-    window = padded_signal(i:i+window_size-1);
-    
-    % Valor da mediana
-    median_value = median(window);
-    filt_photoVector(i) = median_value;   
-end
+%lpFilt_phVec = filtfilt(lpFilt,photoVector);
 
 fig = 1;
-figure (fig); plot(t,photoVector), title('Fotoresistência'), xlabel('Tempo (min)');
-fig = fig + 1;
-Fs = 125; % Sampling frequency
-T = 1/Fs; % Sampling period
-L = length(data(:,3)); % Length of signal
-t = (0:L-1)*T; % Time vector
-Y = fft(data(:,3)); % Fourier transform
+
+% figure (fig); plot(t,photoVector), title('Fotoresistência'), xlabel('Tempo (min)');
+% fig = fig + 1;
+% 
+% figure (fig); plot(t,filt_photoVector), title("Fotoresistência (Mediana flutuante com "+n_pnts+" pontos)"), xlabel('Tempo (min)');
+% fig = fig + 1;
+
+% figure (fig); plot(t,lpFilt_phVec), title("Fotoresistência (Filtro passa-baixo)"), xlabel('Tempo (min)');
+% fig = fig + 1;
+
+%% Re-referenciação do EEG 
+
+eeg_data = data(:,1:end-1);
+
+%car = bsxfun(@minus, eeg_data, mean(eeg_data,2));
+%lap = laplacian_perrinX(eeg_data',[nch_locs.X],[nch_locs.Y],[nch_locs.Z]);
+
+%% Pré-processamento do EEG
+
+% Filtragem
+% De acordo com o artigo com a literatura dos ERPs, um
+% filtro passa-banda entre 0.5 e 30 Hz será usado.
+
+% Band-pass
+bpFilt = designfilt('bandpassfir','FilterOrder',1000, ...
+         'CutoffFrequency1',0.5,'CutoffFrequency2',40, ...
+         'SampleRate',fs); 
+%fvtool(bpFilt) % Visualizar resposta em frequência e fase do filtro
+
+%filt_data = filtfilt(bpFilt,car);
+%filt_data = filtfilt(bpFilt,lap');
+filt_data = filtfilt(bpFilt,eeg_data);
+
+%% FFT do sinal 
+
+% Verificar se o sinal é um EEG. Para isso, o sinal no domíno das
+% frequências tem que seguir a forma 1/f.
+% Isto após retirar as componentes DC e dos 50Hz da rede elétrica.
+
+T = 1/fs; % Sampling period
+L = length(filt_data(:,8)); % Length of signal
+tm = (0:L-1)*T; % Time vector
+Y = fft(filt_data(:,8)); % Fourier transform
 P2 = abs(Y/L); % Two-sided spectrum
 P1 = P2(1:L/2+1); % Single-sided spectrum
 P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(L/2))/L; % Frequency vector
-figure
+f = fs*(0:(L/2))/L; % Frequency vector
+
+figure(fig)
+fig = fig +1;
 plot(f,P1) 
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-figure (fig); plot(t,filt_photoVector), title('Fotoresistência (filtrado)'), xlabel('Tempo (min)');
-fig = fig + 1;
-
-%% Pré-processamento do EEG
-% Filtragem
-% De acordo com o artigo 'A Rapid Face Recognition BCI System using
-% Single-trial ERP', um filtro passa-banda entre 0.5 e 30 Hz será usado
-
-% Band-pass
-bpFilt = designfilt('bandpassfir','FilterOrder',1000, ...
-         'CutoffFrequency1',0.5,'CutoffFrequency2',30, ...
-         'SampleRate',fs); 
-%fvtool(bpFilt) % Visualizar resposta em frequência e fase do filtro
-filt_data = filtfilt(bpFilt,data(:,1:end-1));
+%% Retirar sub-harmónica dos 25 Hz
 
 % Band-stop
 bsFilt = designfilt('bandstopfir','FilterOrder',1000, ...
-         'CutoffFrequency1',24.5,'CutoffFrequency2',25.5, ...
+         'CutoffFrequency1',24.7,'CutoffFrequency2',25.3, ...
          'SampleRate',fs);
 %fvtool(bsFilt)
 filt_data = filtfilt(bsFilt,filt_data);
 
-%% Indices da segmentação do EEG target
-% Sinal da fotoresistência não filtrado 
+%% Deteção dos índices de segmentação
 
-th=250.5;
-targetVector=double((photoVector>th));
+last_low = true; 
+last_high = false;
 
-% locs -> Índice da primeira amostra
-% dur -> duração dos blocos em nº de amostras
-[~,locs,dur,~] = findpeaks(targetVector);
+falling_edges = [];
+rising_edges = [];
 
-figure(fig), histogram((dur/fs)*1000,60), title('Duração dos blocos target'), xlabel('Tempo (s)') % Verificar o tamanho dos blocos (trials)
-fig = fig + 1;
+for i = 1:length(filt_photoVector)
+    photoresistor = filt_photoVector(i);
+    if photoresistor <= 235 % Possible falling edge
+        if last_high % Found falling edge
+            falling_edges = [falling_edges;i];
+        end
+        last_low = true; 
+        last_high = false;
 
-idx = ((dur/fs)*1000 <= 480); % Retira os blocos com nº de amostras/duração inválida (<470 ms)
-dur(idx) = [];
-locs(idx) = [];
-minw = min(dur);
-dur(1:end) = minw;
-
-% Sinal da fotoresistência filtrado 
-
-targetVector=double((filt_photoVector>th));
-
-% locs -> Índice da primeira amostra
-% dur -> duração dos blocos em nº de amostras
-[~,f_locs,f_dur,~] = findpeaks(targetVector);
-
-figure(fig), histogram((f_dur/fs)*1000,60), title('Duração dos blocos target'), xlabel('Tempo (s)') % Verificar o tamanho dos blocos (trials)
-fig = fig + 1;
-
-f_idx = ((f_dur/fs)*1000 <= 480); % Retira os blocos com nº de amostras/duração inválida (<470 ms)
-f_dur(f_idx) = [];
-f_locs(f_idx) = [];
-f_minw = min(f_dur);
-f_dur(1:end) = f_minw;
-
-%shift = abs(locs - f_locs);
-
-f_locs = f_locs - 3; % Ajustar a mudança de fase que ocorre com o filtro de mediana, o qual é constante para
-
-
-
-%% Segmentação do EEG target
-
-targetEEG = [];
-
-for i=1:length(f_dur)
-    targetEEG = [targetEEG; filt_data(f_locs(i)-1:f_locs(i)+f_dur(i),:)];
+    elseif photoresistor >= 230 % Possible rising edge
+        if last_low % Found rising edge
+            rising_edges = [rising_edges; i];
+        end
+        last_low = false;
+        last_high = true; 
+    end
 end
 
-segmented_targetEEG = reshape(targetEEG, f_dur(1)+2, length(f_dur),length(channel_labels));
+rising_edges(1) = [];
 
-%% Indices da segmentação do EEG não-target
+seg_target = [];
+seg_nontarget = [];
 
-th = 160;
-non_targetVector=double((filt_photoVector>th) & ~targetVector);
-
-% locs -> Índice da primeira amostra
-% dur -> duração dos blocos em nº de amostras
-[~,nont_locs,nont_dur,~] = findpeaks(non_targetVector);
-
-figure(fig), histogram((nont_dur/fs)*1000,60), title('Duração dos blocos non-target'), xlabel('Tempo (s)') % Verificar o tamanho dos blocos (trials)
-fig = fig + 1;
-
-idx = ((nont_dur/fs)*1000 <= 480); % Retira os blocos com nº de amostras/duração inválida (<470 ms)
-nont_dur(idx) = [];
-nont_locs(idx) = [];
-minw = min(nont_dur);
-nont_dur(1:end) = minw;
-
-nont_locs = nont_locs - 3;
-
-%% Segmentação do EEG não target
-
-nontargetEEG = [];
-
-for i=1:length(nont_dur)
-    nontargetEEG = [nontargetEEG; filt_data(nont_locs(i)-1:nont_locs(i)+nont_dur(i),:)];
+for i = 1:length(falling_edges) % Verificar quais blocos pertecem aos targets e não targets
+    if(min(filt_photoVector(falling_edges(i):rising_edges(i))) < 140)
+        seg_nontarget = [seg_nontarget, falling_edges(i)];
+    else
+        seg_target = [seg_target, falling_edges(i)];
+    end
 end
 
-segmented_nontargetEEG = reshape(nontargetEEG, nont_dur(1)+2, length(nont_dur),length(channel_labels));
+seg_target = unique(seg_target);
+seg_nontarget = unique(seg_nontarget);
+
+%% Segmentação do EEG
+
+bf = -0.100; % 100 ms antes do estímulo
+af = 0.800; % 800 ms após o estímulo
+
+idx_bf = floor(bf * fs);
+idx_af = ceil(af * fs);
+
+time = ((idx_bf:idx_af)/fs) * 1000; % Vetor do tempo
+
+% Target
+
+[targetEEG, nontargetEEG] = deal(zeros(length(seg_target), length(time), length(channel_labels))); % Pré-alocação dos dados
+
+for i=1:length(seg_target)
+    targetEEG(i,:,:) = cat(3,filt_data((seg_target(i)+idx_bf):(seg_target(i)+idx_af),:));
+end
+
+% Non-target
+
+for i=1:length(seg_nontarget)
+    nontargetEEG(i,:,:) = cat(3,filt_data((seg_nontarget(i)+idx_bf):(seg_nontarget(i)+idx_af),:));
+end
 
 %% Computação dos ERPs
 
-tg_erp = squeeze(mean(segmented_targetEEG, 2)); % Obtenção do ERP target (média do sinal na dimensão trials)
-ntg_erp = squeeze(mean(segmented_nontargetEEG, 2)); % Obtenção do ERP não-target
+tERP = squeeze(mean(targetEEG, 1)); % ERP target (média do sinal na dimensão trials)
+ntERP = squeeze(mean(nontargetEEG, 1)); % ERP não-target
 
-t = (linspace(0,(dur(1)+2)/fs,dur(1)+2)).* 1000;
+%% Correção da linha de base
+
+baseline = [-100;0];
+
+baseidx = dsearchn(time',baseline);
+
+for e=1:length(channel_labels)
+    tmn = mean(tERP(baseidx(1):baseidx(2),e));   
+    ntmn = mean(ntERP(baseidx(1):baseidx(2),e));   
+
+    tERP(:,e) = tERP(:,e) - tmn;
+    ntERP(:,e) = ntERP(:,e) - ntmn;
+end
+
+%% Gráficos
 
 % Definição do tempo dos picos e respetivos índices 
 
-tN170 = [95; 135]; % ms
-idxN170 = dsearchn(t',tN170);
+tN170 = [95; 120]; % ms
+idxN170 = dsearchn(time',tN170);
 
-tN2 = [180; 230];
-idxN2 = dsearchn(t',tN2);
+tN2 = [125; 160];
+idxN2 = dsearchn(time',tN2);
 
-tP3 = [325; 450];
-idxP3 = dsearchn(t',tP3);
+tP3 = [250; 350];
+idxP3 = dsearchn(time',tP3);
 
 % Gráficos dos ERPs no tempo
 
-clim = [floor(min(min(tg_erp))),ceil(max(max(tg_erp)))];
+clim = [floor(min(min(tERP))),ceil(max(max(tERP)))];
 aux = 0;
 
 for i=1:length(channel_labels)
     figure(fig)
     hold on,
     fig = fig + 1;
-    plot(t,tg_erp(:,i),'b','LineWidth',2);
-    plot(t,ntg_erp(:,i),'r','LineWidth',2);
+    plot(time,tERP(:,i),'b','LineWidth',2);
+    plot(time,ntERP(:,i),'r','LineWidth',2);
+    xline(0, '--')    
     ylim([(clim(1)+aux),(clim(2)-aux)])
-    xlim([min(t),max(t)])
-    title(channel_labels(i)), xlabel('Tempo (ms)'), ylabel('Amplitude (\muv)'), yline(0, '--'), grid on
+    xlim([min(time),max(time)])
+    title(channel_labels(i)), xlabel('Tempo (ms)'), ylabel('Amplitude (\muv)'), grid on
     
     if(contains(channel_labels(i),"z") || (contains(channel_labels(i),"C") && ~(contains(channel_labels(i),"P"))) ) % VPP
         c = 'blue';
         a = 'VPP';
-        N2 = [tN2(1)-30, tN2(2)-30, tN2(2)-10, tN2(1)-10];
-    else
+        N2 = [tN2(1), tN2(2), tN2(2), tN2(1)];
+    else % N170
         c = 'cyan';
         a = 'N170';
         N2 = [tN2(1), tN2(2), tN2(2), tN2(1)];
@@ -227,6 +262,9 @@ for i=1:length(channel_labels)
 
     % Estas patches precisam de ser ajustadas
 
+    ph0 = patch([baseline(1) baseline(2) baseline(2) baseline(1)],[clim(1) clim(1) clim(2) clim(2)],'black');
+    set(ph0,'facealpha',.1,'edgecolor','none')
+
     ph1 = patch(N170,[clim(1) clim(1) clim(2) clim(2)],c);
     set(ph1,'facealpha',.1,'edgecolor','none')
 
@@ -236,7 +274,7 @@ for i=1:length(channel_labels)
     ph3 = patch(P3,[clim(1) clim(1) clim(2) clim(2)],'r');
     set(ph3,'facealpha',.1,'edgecolor','none')
 
-    legend('Target','Non-target','Baseline','N170 - VPP', 'N2', 'P3')
+    legend('Target','Non-target','Stimulus','Baseline',a, 'N2', 'P3')
    
     hold off
 end
@@ -248,15 +286,15 @@ fig = fig + 1;
 tlo1=tiledlayout(1,3,'TileSpacing','tight','Padding','tight');
 
 ax1=nexttile(tlo1);
-topoplot(mean(tg_erp(idxN170(1):idxN170(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(tERP(idxN170(1):idxN170(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax1,"N170 - VPP (" + tN170(1) +" - "+ tN170(2) + " ms)"  ,'FontSize',12)
 
 ax2=nexttile(tlo1);
-topoplot(mean(tg_erp(idxN2(1):idxN2(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(tERP(idxN2(1):idxN2(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax2,"N2 (" + tN2(1) +" - "+ tN2(2) + " ms)",'FontSize',12)
 
 ax3=nexttile(tlo1);
-topoplot(mean(tg_erp(idxP3(1):idxP3(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(tERP(idxP3(1):idxP3(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax3,"P3 (" + tP3(1) +" - "+ tP3(2) + " ms)",'FontSize',12)
 
 cbh1=colorbar(ax3);
@@ -269,18 +307,19 @@ fig = fig + 1;
 tlo2=tiledlayout(1,3,'TileSpacing','tight','Padding','tight');
 
 ax4=nexttile(tlo2);
-topoplot(mean(ntg_erp(idxN170(1):idxN170(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(ntERP(idxN170(1):idxN170(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax4,"N170 - VPP (" + tN170(1) +" - "+ tN170(2) + " ms)"  ,'FontSize',12)
 
 ax5=nexttile(tlo2);
-topoplot(mean(ntg_erp(idxN2(1):idxN2(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(ntERP(idxN2(1):idxN2(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax5,"N2 (" + tN2(1) +" - "+ tN2(2) + " ms)",'FontSize',12)
 
 ax6=nexttile(tlo2);
-topoplot(mean(ntg_erp(idxP3(1):idxP3(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
+topoplot(mean(ntERP(idxP3(1):idxP3(2),:)),nch_locs,'maplimits',clim,'numcontour',6,'electrodes','on');
 title(ax6,"P3 (" + tP3(1) +" - "+ tP3(2) + " ms)",'FontSize',12)
 
 cbh1=colorbar(ax6);
 cbh1.Layout.Tile = 'east';
 cbh1.Label.String='\muV';
 cbh1.Label.Rotation=0; 
+
